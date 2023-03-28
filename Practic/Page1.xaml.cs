a67b84cdc9749c491faaecb104808672b211077b
NikitaKustov29 <114473572+NikitaKustov29@users.noreply.github.com>
2023-03-28 08:23:39 +0300

    Add files via upload

---
+++ Practic/Page1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Practic.practicaDataSetTableAdapters;
+
+namespace Practic
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        zizkaTableAdapter zizka = new zizkaTableAdapter();
+        public Page1()
+        {
+            InitializeComponent();
+            ZizkaGrid.ItemsSource = zizka.GetData();
+            ZizkaList.ItemsSource = zizka.GetData();
+            ZizkaList.DisplayMemberPath = "mg";
+        }
+
+        private void ZizkaList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show((ZizkaList.SelectedItem as DataRowView).Row[1].ToString());
+        }
+    }
+}
---
+++ Practic/Page2.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Practic.practicaDataSetTableAdapters;
+
+namespace Practic
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        brandTableAdapter brands = new brandTableAdapter();
+        public Page2()
+        {
+            InitializeComponent();
+            BrandsGrid.ItemsSource = brands.GetData();
+            BrandList.ItemsSource = brands.GetData();
+            BrandList.DisplayMemberPath = "nameBrand";
+        }
+
+        private void BrandList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show((BrandList.SelectedItem as DataRowView).Row[1].ToString());
+        }
+    }
+}
